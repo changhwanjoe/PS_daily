b8fbe10939f9ebf31e210d83161e4f014b89468f
Changhwan Joe <chjoe01@gmail.com>
2023-01-29 16:45:29 +0900

    to be fixed

---
+++ prg_햄버거만들기.py
@@ -0,0 +1,41 @@
+# stack queue --> deque
+from collections import deque
+def solution(ingredient):
+    answer = 0
+    prev = 0
+    d = deque()
+    for elm in ingredient:
+        if elm ==1 :
+            if len(d)==0:
+                d.append(elm)
+            else: # 데크 스택에 뭔가 있는경우 
+                if d[-1]== 3:
+                    for _ in range(3):
+                        d.pop()
+                    answer+=1
+                else: d.append(elm)
+        else: # 2 or 3인경우
+            if len(d) == 0:
+                continue
+            if elm ==2 : 
+                if d[-1]== 1:
+                    d.append(elm)
+                else : # 2 or 3
+                    clear_stack(d)
+            if elm ==3 :
+                if d[-1] == 2:
+                    d.append(elm)
+                else : clear_stack(d)
+    return answer
+
+def clear_stack(d):
+    while True:
+        a = d.pop()
+        if a==1:break
+
+
+if __name__=="__main__":
+    ingredient= [2, 1, 1, 2, 3, 1, 2, 3, 1]
+    print(solution(ingredient))
+    ingredient= [1, 3, 2, 1, 2, 1, 3, 1, 2]
+    print(solution(ingredient))
