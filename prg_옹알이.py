06c3abfdb3c610dad5563b0eb07a333586283735
Changhwan Joe <chjoe01@gmail.com>
2023-01-30 15:05:27 +0900

    Update prg_옹알이.py

--- prg_옹알이.py
+++ prg_옹알이.py
@@ -1,23 +1,23 @@
 def solution(babbling):
     answer = 0
-    for word in babbling:
-        if check(word):
-            answer+=1 
-    return answer
-
-def check(word):
     babbling_list = ["aya", "ye", "woo", "ma"]
-    while len(word)>0:
-        for elm in babbling_list:
-            if word.startswith(elm):
-                word = word[len(elm):]
-                babbling_list.remove(elm)
-                break
+    answer =0 
+    for word in babbling:
+        trig = True
+        for babb in babbling_list:
+            word = word.replace(babb,"@")
         
-
+        for ch in word:
+            if ch!="@": 
+                trig= False
+                break
+        if trig : answer+=1 
+         
+    return answer
 
 
 if __name__=="__main__":
     babbling = ["aya", "yee", "u", "maa", "wyeoo"]
+    print(solution(babbling))
     babbling = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]
-    solution(babbling)
+    
