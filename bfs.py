43dc5623ac05923360e7bdc3aaae8290f85fb056
Changhwan Joe <chjoe01@gmail.com>
2023-02-05 23:26:03 +0900

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -11,15 +11,37 @@ def bfs(v):
                 stack.append(w)
                 visited.append(w)
 
-def bfs(graph, start, visited):
-    visited =[start]
-    stack=deque([start])
+
+def dfs(start,graph, visited):
+    q = deque([start])
+    visited = [start]
+    while q :
+        v = q.popleft()
+        for w in graph[w]:
+            if w not in visited:
+                q.append(w)
+                visited.append(w)
+
+def dfs(start,graph, visited):
+    visited = []
+    stack = deque([start])
     while stack:
-        v = stack.popleft()
+        v = stack.pop()
+        visited.append(v)
         for w in graph[v]:
             if w not in visited:
+                stack.append(w)       
+
+def dfs_iterative(start_node,graph):
+    discovered = []
+    stack = deque()
+    stack.append(start_node)
+
+    while stack:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.append(v)
+            for w in graph[v]:
                 stack.append(w)
-                visited.append(w)
-    return visited
-
-
+                
+    return discovered
