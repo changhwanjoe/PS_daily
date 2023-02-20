f83c29fb81a65e44402f0ef8029a082c4f587af7
ch <chjoe01@gmail.com>
2023-02-21 08:54:05 +0900

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -22,7 +22,7 @@ def dfs(start,graph, visited):
                 q.append(w)
                 visited.append(w)
 
-def dfs(start,graph, visited):
+def bfs(start,graph, visited):
     visited = []
     stack = deque([start])
     while stack:
@@ -32,11 +32,11 @@ def dfs(start,graph, visited):
             if w not in visited:
                 stack.append(w)       
 
-def dfs_iterative(start_node,graph):
+
+def dfs_iterative(start_node,graph): # 파이썬 알고리즘 인터뷰 
     discovered = []
     stack = deque()
     stack.append(start_node)
-
     while stack:
         v = stack.pop()
         if v not in discovered:
@@ -45,3 +45,17 @@ def dfs_iterative(start_node,graph):
                 stack.append(w)
                 
     return discovered
+
+def dfs_recursive(v,discovered=[]): # 파이썬 알고리즘 인터뷰
+    discovered.append(v)
+    for w in graph[v]:
+        if w not in discovered:
+            discovered = dfs_recursive(w,discovered)
+    return discovered
+
+def dfs(graph,v, visited): # 나동빈
+    visited[v] = True
+    print(v,end ='')
+    for i in graph[i]:
+        if not visited[i]:
+            dfs(graph,i,visited)
