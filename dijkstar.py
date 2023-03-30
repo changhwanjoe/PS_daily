66736a97882882a31a84e6400a240b7aad024e17
ch <chjoe01@gmail.com>
2023-03-30 11:37:45 +0900

    Update dijkstar.py

--- dijkstar.py
+++ dijkstar.py
@@ -6,7 +6,9 @@ n,m = map(int, input().split())
 start = int(input())
 graph = [[] for _ in range(n+1)]
 # 최단거리 1차원 테이블 초기화
-dist = [INF]*(n+1)
+distance = [INF]*(n+1)
+
+# 
 
 for _ in range(m):
     a,b,c = map(int, input().split())
@@ -16,14 +18,16 @@ for _ in range(m):
 def dijkstra(start):
     q = []
     heapq.heappush(q,(0,start))
-    visited = []
-    
-    for destination, distance in graph[start]: 
-        heapq.heappush(q, (distance, destination))
-
+    dist[start] = 0 
 
     while q :
-        dist, now = heapq.heappop()
-        if dist
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + 1 
+            if cost < distance[i[0]]:
+                heapq.heappush(q,(cost,i[1]))
+
 
 
