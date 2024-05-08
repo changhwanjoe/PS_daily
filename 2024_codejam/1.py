a9effedce304d616f80ab737fcf72e433a26f4d8
changhwan <chjoe01@gmail.com>
2024-05-09 08:31:40 +0900

    0509 added

---
+++ 2024_codejam/1.py
@@ -0,0 +1,39 @@
+import sys
+readl = sys.stdin.readline
+ 
+T = int(readl())
+ 
+for _ in range(T):
+    N = int(readl())
+ 
+    D = list(map(int,readl().split()))
+    S = list(readl())[:-1]
+    R = list(map(int,readl().split()))
+ 
+    right = 10**13+1
+    left = 1
+    mid = (left + right) //2
+ 
+    while left != right:
+        HP = mid
+ 
+        for n in range(N):
+            HP += -D[n]
+ 
+            if HP<=0 or HP >10**13+1:
+                break
+ 
+            if S[n] == '+':
+                HP += R[n]
+            else:
+                HP *= R[n]
+ 
+        if HP <= 0:
+            left = mid +1
+ 
+        else:
+            right =mid
+ 
+        mid = (left + right)//2
+ 
+    print(right)  
