226fbbc919e7f9b7b1002d7a5b6e4db7ab5e0bc8
changhwan.joe <changhwan.joe@lge.com>
2024-09-01 19:26:50 +0900

    daily

---
+++ BOJ/11724.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,M;
+    cin>>N>>M;
+    for (int i=0; i<M; i++){
+        int u,v;
+        cin >> u >> v;
+    }
+    return 0;
+}
