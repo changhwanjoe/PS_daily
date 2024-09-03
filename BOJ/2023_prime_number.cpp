ae615e1f3234b232fb68f57d8fcfef613e30b947
changhwan joe <chjoe01@gmail.com>
2024-09-03 23:54:18 +0900

    daily

---
+++ BOJ/2023_prime_number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+void DFS(int a, int jaritsu);
+bool isPrime(int num);
+
+void DFS(int a, int jaritsu){
+
+
+    
+}
+
+bool isPrime(int num){
+    
+}
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for ()
+
+    int N,M;
+    cin>>N>>M;
+    for (int i=0; i<M; i++){
+        int u,v;
+        cin >> u >> v;
+    }
+    return 0;
+}
