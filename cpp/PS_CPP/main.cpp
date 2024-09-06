384cedf6a8e371f0ddc983b43975f947221a235c
Changhwan Joe <chjoe01@gmail.com>
2024-09-06 21:21:13 +0900

    daily

---
+++ cpp/PS_CPP/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+	vector<int> A(N); // A ¿Ã∏ß + 
+
+	return 0;
+}
