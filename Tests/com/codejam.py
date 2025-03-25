57257b14cd125e6a23c27a0f16fe806e71365537
changhwan.joe <chjoe01@gmail.com>
2025-03-25 23:35:12 +0900

    git ignore

---
+++ Tests/com/codejam.py
@@ -0,0 +1,10 @@
+import sys
+
+#input = sys.stdin.readline
+T = int(input().split())
+
+for i in range(T):
+    N, X = input().split()
+    N = int(N)
+    M = len(X)
+    V = list(map(int,input().split()))
