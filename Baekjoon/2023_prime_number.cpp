3be493244776d3a9655dff9679802d454c3952f5
Changhwan Joe <chjoe01@gmail.com>
2026-06-11 09:12:02 +0900

    2023

--- Baekjoon/2023_prime_number.cpp
+++ Baekjoon/2023_prime_number.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 using namespace std;
 
-
-void DFS(int a, int jaritsu);
+void DFS(int number, int jaritsu);
 bool isPrime(int num);
-
-void DFS(int a, int jaritsu){
-
-
-    
-}
-
-bool isPrime(int num){
-    
-}
+static int N;
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    for ()
+    cin >> N;
 
-    int N,M;
-    cin>>N>>M;
-    for (int i=0; i<M; i++){
-        int u,v;
-        cin >> u >> v;
-    }
+    DFS(2,1);
+    DFS(3,1);
+    DFS(5,1);
+    DFS(7,1); 
+   
     return 0;
+}
+
+void DFS(int number, int jaritsu){
+    if (jaritsu == N){
+        if (isPrime(number)){
+            cout << number << "\n";
+        }
+        return;
+    }
+    for (int i =1; i<10; i+=2){ 
+        if (isPrime(number*10 + i)){              
+            DFS(number*10 + i, jaritsu+1);
+        }
+    }
+    return;
+    }
+
+bool isPrime(int num){
+    for (int i =2; i<num; i++){
+        if (num % i == 0){
+            return false;
+        }
+    }
+    return true;
 }
