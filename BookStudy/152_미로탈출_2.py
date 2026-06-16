a40073bb3f5cd6c2cea7c242d9ebdbf729e14d14
changhwan.joe <chjoe01@gmail.com>
2026-06-16 22:18:11 +0900

    152bfs

--- BookStudy/152_미로탈출_2.py
+++ BookStudy/152_미로탈출_2.py
@@ -1,8 +1,9 @@
 from collections import deque
-n,m = map(int, input().split())
-graph  = []
-for i in range(n):
-    graph.append(list(map(int,input())))
+
+N, M = map(int,input().split())
+graph = []
+for i in range(M):
+    graph.append(list(map(int, input())))
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
@@ -11,20 +12,23 @@ def bfs(x,y):
     q = deque()
     q.append((x,y))
     count = 0
+    visited= []
     while q:
-        count+=1 
         tx,ty = q.popleft()
         for i in range(4):
             nx = tx + dx[i]
-            ny = ty + dy[i]
-
-            if 0<=nx<n and 0<=ny<m and graph[nx][ny]!=0:
-                if graph[nx][ny] == 1:
-                    q.append((nx,ny))
-                    graph[nx][ny] = graph[x][y] + 1
-
-    return graph[n-1][m-1]
+            ny = ty + dy[i]         
+            if nx < 0 or nx>=N or ny <0 or ny >= M:
+                continue
+            if graph[nx][ny] == 0:
+                continue 
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[tx][ty] +1 
+                q.append((nx,ny))
+    return graph[N-1][M-1]
 print(bfs(0,0))
 
 
 
+#def bfs(int v, start, visited):
+    
