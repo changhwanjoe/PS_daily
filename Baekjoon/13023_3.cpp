90d9186cdb0ebb7e1b70ba6dd9063b480d0a2814
Changhwan Joe <chjoe01@gmail.com>
2026-06-16 10:35:07 +0900

    0616

--- Baekjoon/13023_3.cpp
+++ Baekjoon/13023_3.cpp
@@ -5,10 +5,24 @@ using namespace std;
 
 static vector<vector<int>> A;
 static vector<bool> visited;
+<<<<<<< HEAD:Baekjoon/13023.cpp
 static bool arrived;
 void DFS(int i, int depth);
 int main(void)
 {
+=======
+void DFS(int number, int depth);
+void DFS(int number, int depth){
+    if (depth == 5){
+        cout << 1 << "\n";
+        return;
+    }
+    for (int i=0; i<friend_list.size(); i++){
+        
+    }
+})
+int main(void){
+>>>>>>> bbfdc3e (0615):Baekjoon/13023_2.cpp
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
