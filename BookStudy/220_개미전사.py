cf60324c4cae2d4efc247dd02ce409d9039c882c
changhwan.joe <chjoe01@gmail.com>
2026-06-18 21:37:19 +0900

    dp 개미전사

--- BookStudy/220_개미전사.py
+++ BookStudy/220_개미전사.py
@@ -1,1 +1,12 @@
-220_개미전사.py
+N = int(input())
+array = list(map(int,input().split()))
+
+
+d = [-1]*(N)
+d[0] = array[0]
+d[1] = max(array[1],array[0])
+
+for i in range(2,N):
+    d[i] = max(array[i]+d[i-2], d[i-1])
+
+print(d[N-1])
